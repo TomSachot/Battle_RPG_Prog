1462e8c4c9b3de3ecc33b89232b930bef2cf354c
TomSachot <74669910+TomSachot@users.noreply.github.com>
2020-12-20 16:20:02 +0100

    Create Battle_RPG.js

---
+++ Battle_RPG.js
@@ -0,0 +1,26 @@
+var atk = document.getElementById("Attaque1")
+var def = document.getElementById("Défense1")
+var ulti1 = document.getElementById("Actionperso1")
+var pvmonstre = document.getElementById("HPmonstre")
+var pvperso1 = document.getElementById("affichePV")
+var texte = document.getElementById("lw")
+var manaperso1 = 30
+var atkperso1 = 24
+var defperso1 = 0.5
+var tourulti1 = 3
+var atkmonstre = 13
+
+pvmonstre.value = 250
+pvmonstre.innerHTML = pvmonstre.value
+pvperso1.value = 100
+pvperso1.innerHTML = pvperso1.value
+
+
+atk.onclick = function(){
+    texte.innerHTML = "Vous causez des dégâts au Révélator ! Quelle est votre prochaine action ?";
+    pvmonstre.value = pvmonstre.value-atkperso1;
+    pvmonstre.innerHTML = pvmonstre.value;
+    pvperso1 = pvperso1 - atkmonstre;
+    document.getElementById("affichePV").innerHTML=pvperso1;    
+}
+
