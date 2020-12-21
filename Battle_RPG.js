2ea7a4c76329e148f9873fea2a6a07b83e58032e
TomSachot <74669910+TomSachot@users.noreply.github.com>
2020-12-21 16:11:18 +0100

    Update Battle_RPG.js

--- Battle_RPG.js
+++ Battle_RPG.js
@@ -3,17 +3,21 @@ var def = document.getElementById("Défense1")
 var ulti1 = document.getElementById("Actionperso1")
 var pvmonstre = document.getElementById("HPmonstre")
 var pvperso1 = document.getElementById("affichePV")
+var manaperso1 = document.getElementById("afficheMana")
 var texte = document.getElementById("lw")
-var manaperso1 = 30
-var atkperso1 = 24
+var coutmana = 9
+var atkperso1 = 27
 var defperso1 = 0.5
-var tourulti1 = 3
-var atkmonstre = 13
+var atkmonstre = 12
 
 pvmonstre.value = 250
 pvmonstre.innerHTML = pvmonstre.value
 pvperso1.value = 100
 pvperso1.innerHTML = pvperso1.value
+manaperso1.value = 30
+manaperso1.innerHTML = manaperso1.value
+atkperso1.value = 27
+atkmonstre.value = 12
 
 
 atk.onclick = function(){
@@ -21,11 +25,37 @@ atk.onclick = function(){
     pvmonstre.value = pvmonstre.value-atkperso1;
     pvmonstre.innerHTML = pvmonstre.value;
     pvperso1.value = pvperso1.value-atkmonstre;
-    pvperso1.innerHTML = pvperso1.value; 
+    pvperso1.innerHTML = pvperso1.value;
+    if (pvperso1.value <= 0){
+        texte.innerHTML = "Le Révelator est trop puissant pour vous...";
+        pvperso1.innerHTML = "0";
+    }
+    if (pvmonstre.value <= 0){
+        texte.innerHTML = "Rentrez faire votre rapport, la région est sauvée !";
+        document.getElementById("Monstre1").style.backgroundImage = "url(fumee.png)";
+        pvmonstre.innerHTML = "0";
+    }
 }
 def.onclick = function(){
     texte.innerHTML = "Vous êtes à couvert mais aussi légèrement blessé... Quelle est votre prochaine action ?"
     pvperso1.value = pvperso1.value-(atkmonstre*defperso1);
     pvperso1.innerHTML = pvperso1.value;
+    if (pvperso1.value <= 0){
+        texte.innerHTML = "Le Révelator est trop puissant pour vous...";
+        pvperso1.innerHTML = "0";
+    }
+}
+ulti1.onclick = function(){
+    texte.innerHTML = "Eli se soigne avec un kit de soins !"
+    pvperso1.value = pvperso1.value + 23;
+    pvperso1.innerHTML = pvperso1.value;
+    manaperso1.value = manaperso1-coutmana;
+    manaperso1.innerHTML = manaperso1.value;
+    if (pvperso1.value > 100){
+        pvperso1.value = 100;
+        pvperso1.innerHTML = pvperso1.value;
+    }
+
+
 }
 
